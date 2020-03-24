833fef6c4ab6bd3b6daa5bb5d5ccf0f8febf7bd9
ekate <ekatereanagricaenko@gmail.com>
2020-03-24 13:03:37 +0200

    add registration tests

--- test/routes.auth.test.js
+++ test/routes.auth.test.js
@@ -11,6 +11,7 @@ chai.use(chaiHttp)
 const server = require('../src/server/app')
 const knex = require('../src/server/db/connection')
 
+// common view for chai-describe
 describe('routes : auth', () => {
   beforeEach(() => {
     return knex.migrate.rollback()
@@ -23,11 +24,42 @@ describe('routes : auth', () => {
   })
 })
 
+// get request to register user
 describe('GET /auth/register', () => {
+	// what shoul be done before executing (not server part)
 	it('should render the register view', (done) => {
 		chai.request(server)
 		.get('/auth/register')
-		.end((err, res))
+		// what we should get after executing get request
+		.end((err, res) => {
+			should.not.exist(err)
+			res.redirects.length.should.eql(0)
+			res.status.should.eql(200)
+			res.type.should.eql("text/html")
+			// this requements will be chanched.
+			// res.text.should.contain('<h1>Register</h1>')
+			// res.text.should.contain(
+			// 	'<p><button type="submit">Register</button>')
+			done()
+
+		})
+	})
+})
+
+// post request to register user
+despribe('POST /auth/register', () => {
+	it('should register a new user', (done) => {
+		chai.request(server)
+		.post('/auth/register')
+		.send({
+			username: 'unicorn',
+			password: 'my_little_pony'
+		})
+		.end((err, res) => {
+			should.not.exists(err)
+			res.redirects[0].should.contain('/home')
+			done()
+		})
 	})
 })
 
