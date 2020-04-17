119b0125020ae74000b0cb6fe2891db4d389d9bd
vermi4elli <nikita.molodtsov21@gmail.com>
2020-04-17 19:23:00 +0300

    Added the logo and it's animation

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -3,10 +3,13 @@ import React from 'react';
 import './Header.css';
 // eslint-disable-next-line no-unused-vars
 import Menu from './../Menu/Menu';
+// eslint-disable-next-line no-unused-vars
+import Logo from './../Logo/Logo';
 
 function Header () {
   return (
     <div className="Header">
+      <Logo className="Logo"/>
       <Menu className="Menu"/>
     </div>
   );
---
+++ frontend/src/Components/Logo/Logo.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Logo.css';
+
+function Logo () {
+  return (
+    <div className="Logo">
+      <p className="Text">Portfolio constructor</p>
+    </div>
+  );
+}
+
+export default Logo;
