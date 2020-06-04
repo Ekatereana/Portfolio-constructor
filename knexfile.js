b9ca91167d19b447f3f112fce10bea89e949c5e0
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-06-04 11:20:40 +0300

    Added the fixed

--- knexfile.js
+++ knexfile.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.join(BASE_PATH, 'migrations')
     },
