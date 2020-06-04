54422777d42f1c73aed3aea4df952aef9b0a691e
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-06-04 11:02:42 +0300

    Fixed start

--- knexfile.js
+++ knexfile.js
@@ -25,6 +25,16 @@ module.exports = {
     seeds: {
       directory: path.join(BASE_PATH, 'seeds')
     }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DB_URL + '?ssl=true',
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations')
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds')
+    },
+    ssl: true
   }
-
 };
